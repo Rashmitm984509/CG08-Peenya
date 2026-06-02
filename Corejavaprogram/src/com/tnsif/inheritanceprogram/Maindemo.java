6fe21a9c419817a3441ca315f6a2659eb6754e87
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2026-06-02 11:57:03 +0530

    commit

--- Corejavaprogram/src/com/tnsif/inheritanceprogram/Maindemo.java
+++ Corejavaprogram/src/com/tnsif/inheritanceprogram/Maindemo.java
@@ -1,5 +1,5 @@
 package com.tnsif.inheritanceprogram;
-
+//demo for inheritance
 class Father{
 	int money=90000;
 	String car="bmw";
@@ -17,6 +17,8 @@ class Son extends Father{
 }
 public class Maindemo {
 public static void main(String[] args) {
+	Father f=new Father();
+	f.drinking();
 	Son s=new Son();
 	System.out.println(s.money);
 	System.out.println(s.car);
---
+++ Corejavaprogram/src/com/tnsif/poymorphismdemo/PaymentTest.java
@@ -0,0 +1,30 @@
+package com.tnsif.poymorphismdemo;
+// demo for method overriding
+class Payment{
+	void makepayment() {
+		System.out.println("processing payment using method");
+	}
+}
+//subclass
+class UPIPayment extends Payment{
+	@Override
+	void makepayment() {
+		System.out.println("payment using UPI");
+	}	
+}
+class CardPayment extends Payment{
+	@Override
+	void makepayment() {
+		System.out.println("payment using credit card");
+	}	
+}
+
+public class PaymentTest {
+public static void main(String[] args) {
+	UPIPayment u=new UPIPayment();
+	u.makepayment();
+	
+	CardPayment v=new CardPayment();
+	v.makepayment();
+}
+}
